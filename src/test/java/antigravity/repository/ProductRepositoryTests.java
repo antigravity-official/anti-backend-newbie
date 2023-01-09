b68b35f5d7a9f8af86ad22decf5e6aaf0f1ec2a7
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-09 12:24:16 +0900

    feat : add product repository test

--- src/test/java/antigravity/repository/ProductRepositoryTests.java
+++ src/test/java/antigravity/repository/ProductRepositoryTests.java
@@ -1,24 +1,27 @@
 package antigravity.repository;
 
-import antigravity.entity.Product;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import antigravity.entity.Product;
+
 @SpringBootTest
 @Transactional
 public class ProductRepositoryTests {
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    @Test
-    public void findByIdTest() {
-        Long id = 1L;
-        Product product = productRepository.findById(id);
-        Assertions.assertNotNull(product);
-    }
+	@Test
+	public void findByIdTest() {
+		Long id = 1L;
+		Optional<Product> optionalProduct = productRepository.findById(id);
+		Assertions.assertTrue(optionalProduct.isPresent());
+	}
 
 }
