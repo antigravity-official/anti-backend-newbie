162eafc150188acc75ed927c67d642ba07f25f89
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-10 01:21:49 +0900

    feat : change query and add param

--- src/main/java/antigravity/repository/ProductLikeRepository.java
+++ src/main/java/antigravity/repository/ProductLikeRepository.java
@@ -3,12 +3,16 @@ package antigravity.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import antigravity.entity.Product;
 import antigravity.entity.ProductLike;
 import antigravity.entity.User;
 
 public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
-	Optional<ProductLike> findByProductAndUser(Product product, User user);
+
+	@Query("SELECT pl FROM ProductLike pl WHERE pl.product = :product AND pl.user = :user")
+	Optional<ProductLike> findByProductAndUser(@Param("product") Product product, @Param("user") User user);
 
 }
--- src/main/java/antigravity/repository/ProductRepository.java
+++ src/main/java/antigravity/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import antigravity.entity.Product;
 import antigravity.entity.User;
@@ -11,9 +12,9 @@ import antigravity.entity.User;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	@Query("SELECT p FROM Product p JOIN ProductLike pl ON pl.product = p WHERE pl.user = :user AND p.deletedAt is null")
-	Page<Product> findAllLikeProduct(User user, Pageable pageable);
+	Page<Product> findAllLikeProduct(@Param("user") User user, Pageable pageable);
 
-	@Query("SELECT p FROM Product p JOIN ProductLike pl ON pl.product = p WHERE pl.user <> :user AND p NOT IN (SELECT p FROM p WHERE pl.user = :user) AND p.deletedAt is null")
-	Page<Product> findAllNotLikeProduct(User user, Pageable pageable);
+	@Query("SELECT p FROM Product p WHERE p NOT IN (SELECT pl.product FROM ProductLike pl WHERE pl.user = :user) AND p.deletedAt is null")
+	Page<Product> findAllNotLikeProduct(@Param("user") User user, Pageable pageable);
 
 }
