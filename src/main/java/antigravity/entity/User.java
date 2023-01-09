995090b2039df1e35d8d042026ffee004b101ec8
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-09 14:23:22 +0900

    feat : add isDeleted
    삭제되었는지 검사하는 로직 추가

--- src/main/java/antigravity/entity/User.java
+++ src/main/java/antigravity/entity/User.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -14,7 +15,7 @@ import lombok.Getter;
 public class User extends BaseEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(length = 120, nullable = false)
@@ -24,4 +25,8 @@ public class User extends BaseEntity {
 	private String name;
 
 	private LocalDateTime deletedAt;
+
+	public boolean isDeleted() {
+		return deletedAt != null;
+	}
 }
