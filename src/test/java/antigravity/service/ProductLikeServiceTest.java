44305107d2829a3f02331d3569b0907455996797
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-09 14:43:37 +0900

    feat : add product like exception test
    예외 테스트 추가

--- src/test/java/antigravity/service/ProductLikeServiceTest.java
+++ src/test/java/antigravity/service/ProductLikeServiceTest.java
@@ -55,4 +55,62 @@ public class ProductLikeServiceTest {
 		assertDoesNotThrow(() -> productLikeService.productLike(productId, userId));
 	}
 
+	@DisplayName("찜 상품 등록 테스트 - 유요하지 않은 productId 일때")
+	@Test
+	void given_InvalidProductId_when_ProductLike_then_ThrowsIllegalStateException() {
+		//given
+		Long productId = 1L;
+		Integer userId = 1;
+
+		//when
+		when(productService.findProductById(productId)).thenThrow(new IllegalStateException());
+
+		//then
+		assertThrows(IllegalStateException.class,
+			() -> productLikeService.productLike(productId, userId));
+	}
+
+	@DisplayName("찜 상품 등록 테스트 - 유요하지 않은 userId 일때")
+	@Test
+	void given_InvalidUserId_when_ProductLike_then_ThrowsIllegalStateException() {
+		//given
+		Long productId = 1L;
+		Integer userId = 1;
+
+		//mock
+		Product mockedProduct = mock(Product.class);
+
+		//when
+		when(productService.findProductById(productId)).thenReturn(mockedProduct);
+		when(userService.findUserById(userId)).thenThrow(new IllegalStateException());
+
+		//then
+		assertThrows(IllegalStateException.class,
+			() -> productLikeService.productLike(productId, userId));
+	}
+
+	@DisplayName("찜 상품 등록 테스트 - 이미 찜한 상품일 때")
+	@Test
+	void given_AlreadyProductLike_when_ProductLike_then_ThrowsIllegalStateException() {
+		//given
+		Long productId = 1L;
+		Integer userId = 1;
+
+		//mock
+		Product mockedProduct = mock(Product.class);
+		User mockedUser = mock(User.class);
+		ProductLike mockedProductLike = mock(ProductLike.class);
+
+		//when
+		when(productService.findProductById(productId)).thenReturn(mockedProduct);
+		when(userService.findUserById(userId)).thenReturn(mockedUser);
+		when(productLikeRepository.findByProductAndUser(mockedProduct, mockedUser)).thenReturn(
+			Optional.of(mockedProductLike));
+
+		//then
+		IllegalStateException e = assertThrows(IllegalStateException.class,
+			() -> productLikeService.productLike(productId, userId));
+		assertEquals("Already User Liked Product", e.getMessage());
+	}
+
 }
