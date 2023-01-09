381b94673237848ad4ad2670583d2face6ac8b40
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-09 13:52:15 +0900

    test : save product like test
    찜 상품 등록 테스트
    
    __비즈니스로직__
    1. 올바른 product id 인지 검사
    2. 올바른 user id 인지 검사
    3. 이미 찜한 상품인지 검사
    4. 찜 상품 등록 DB 저장
    5. 상품 조회수 1 증가

---
+++ src/test/java/antigravity/service/ProductLikeServiceTest.java
@@ -0,0 +1,59 @@
+package antigravity.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import antigravity.entity.Product;
+import antigravity.entity.ProductLike;
+import antigravity.entity.User;
+import antigravity.repository.ProductLikeRepository;
+
+@ExtendWith(MockitoExtension.class)
+
+public class ProductLikeServiceTest {
+
+	@Mock
+	private ProductLikeRepository productLikeRepository;
+
+	@Mock
+	private UserService userService;
+
+	@Mock
+	private ProductService productService;
+
+	@InjectMocks
+	private ProductLikeService productLikeService;
+
+	@DisplayName("찜 상품 등록 테스트")
+	@Test
+	void given_ProductIdAndUserId_when_productLike_then_DoesThrow() {
+		//given
+		Long productId = 1L;
+		Integer userId = 1;
+
+		//mock
+		Product mockedProduct = mock(Product.class);
+		User mockedUser = mock(User.class);
+		ProductLike mockedProductLike = mock(ProductLike.class);
+
+		//when
+		when(productService.findProductById(productId)).thenReturn(mockedProduct);
+		when(userService.findUserById(userId)).thenReturn(mockedUser);
+		when(productLikeRepository.findByProductAndUser(mockedProduct, mockedUser)).thenReturn(Optional.empty());
+		when(productLikeRepository.save(any(ProductLike.class))).thenReturn(mockedProductLike);
+		doNothing().when(mockedProduct).increaseViewed();
+
+		//then
+		assertDoesNotThrow(() -> productLikeService.productLike(productId, userId));
+	}
+
+}
