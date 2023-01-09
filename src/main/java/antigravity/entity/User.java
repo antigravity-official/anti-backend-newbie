40a0a7f99e2ef6856f0a88011bd5b9defdab7810
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-09 19:05:14 +0900

    feat : add equals and hashcode
    값 비교를 위해 추가

--- src/main/java/antigravity/entity/User.java
+++ src/main/java/antigravity/entity/User.java
@@ -1,6 +1,7 @@
 package antigravity.entity;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -29,4 +30,20 @@ public class User extends BaseEntity {
 	public boolean isDeleted() {
 		return deletedAt != null;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof User))
+			return false;
+		User user = (User)o;
+		return id.equals(user.id) && email.equals(user.email) && name.equals(user.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, email, name);
+	}
+
 }
