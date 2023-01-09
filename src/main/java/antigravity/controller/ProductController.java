362ea6e3451d70aef504df4e555b1e6139932fea
이상우 <mungjin4966@gmail.com>
2023-01-09 20:16:20 +0900

    feat: Controller api 함수 추가

--- src/main/java/antigravity/controller/ProductController.java
+++ src/main/java/antigravity/controller/ProductController.java
@@ -1,9 +1,37 @@
 package antigravity.controller;
 
+import antigravity.entity.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 
     // TODO 찜 상품 등록 API
+    @PostMapping("/liked/{productId}")
+    public ResponseEntity likedCreate(@PathVariable int productId){
+        System.out.println("produtID : " + productId);
+
+        return ResponseEntity.ok().body("등록이 완료 되었습니다.");
+
+    }
+
 
     // TODO 찜 상품 조회 API
+    @GetMapping()
+    public void productView(@RequestParam("liked") boolean liked,
+                               @RequestParam("page") int page,
+                               @RequestParam("size") int size){
+
+        System.out.println("value : " + liked);
+        System.out.println("page : " + page);
+        System.out.println("size : " + size);
+    }
+
+
 
 }
