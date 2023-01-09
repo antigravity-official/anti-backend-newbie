57eae28a1f9650af5baca72ca3dae31d159125a2
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-01-09 16:51:44 +0900

    setting(jpa) : add jpa setting

---
+++ src/main/java/antigravity/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package antigravity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+@Configuration
+@EnableJdbcAuditing
+public class JpaConfig {
+}
