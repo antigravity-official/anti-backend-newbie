5ba668ddc1aa7033f5c4553d513033c93342f346
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2023-01-10 18:46:18 +0900

    feat : remove cache
    상품 캐싱 제거
    현재 로직으로는 변화가 많이 일어난다.(찜 등록 시 조회수 증가)
    즉, 캐싱 데이터의 동기화 작업이 많이 필요해져 캐싱을 제거하였다.
    
    조회수를 제거하면 캐싱을 사용할 수 있을 거 같다.(product 의 변화가 등록을 제외하고는 없으므로)
    조회수 컬럼을 없애면 product_like 테이블에서 아이템의 갯수를 세주어야 한다. 즉, 매 요청시 추가적인 쿼리가 발생한다.
    
    캐싱 동기화 비용 vs 추가 쿼리 비용 어떤것을 선택해야할지 모르겠다.

--- src/main/java/antigravity/repository/ProductRepository.java
+++ src/main/java/antigravity/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package antigravity.repository;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,16 +11,10 @@ import antigravity.entity.User;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-	@Cacheable(value = "product_like")
 	@Query("SELECT p FROM Product p JOIN ProductLike pl ON pl.product = p WHERE pl.user = :user")
 	Page<Product> findAllLikeProduct(@Param("user") User user, Pageable pageable);
 
-	@Cacheable(value = "product_unlike")
 	@Query("SELECT p FROM Product p WHERE p NOT IN (SELECT pl.product FROM ProductLike pl WHERE pl.user = :user)")
 	Page<Product> findAllUnlikeProduct(@Param("user") User user, Pageable pageable);
-
-	@Cacheable(value = "product_all")
-	@Override
-	Page<Product> findAll(Pageable pageable);
 
 }
