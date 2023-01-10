a8a74288b6a31d32c0021f85fd2c5e7cfac551df
김경민 <nutmy52@naver.com>
2023-01-10 22:31:55 +0900

    feat: Product Entity design
    물품 엔티티 설계

--- src/test/java/antigravity/repository/ProductRepositoryTests.java
+++ src/test/java/antigravity/repository/ProductRepositoryTests.java
@@ -1,9 +1,6 @@
 package antigravity.repository;
 
-import antigravity.entity.Product;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,14 +8,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ProductRepositoryTests {
 
-    @Autowired
-    private ProductRepository productRepository;
-
     @Test
     public void findByIdTest() {
-        Long id = 1L;
-        Product product = productRepository.findById(id);
-        Assertions.assertNotNull(product);
     }
 
 }
