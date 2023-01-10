fb120450320fdd21b9ac61ccb4348a1782d3a906
algosipdahack <sophia5460@ewhain.net>
2023-01-10 21:00:08 +0900

    ✅ test: userRepositoryTest 완료

---
+++ src/test/java/antigravity/user/repository/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package antigravity.user.repository;
+
+import antigravity.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+    User user;
+    @BeforeEach
+    void settings() {
+        user = User.builder()
+                .name("조소연")
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+    @Test
+    @DisplayName("유저정보 저장 후 찾기")
+    void saveAndFind() {
+        //when
+        Long id = userRepository.save(user);
+        User findUser = userRepository.findById(id).get();
+
+        //then
+        assertEquals(id, findUser.getId());
+    }
+
+}
