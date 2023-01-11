feebce62a4dd268bd177de90aef41bb8465f9516
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-01-11 14:28:11 +0900

    fid(NotFoundProductException) : 에러 메세지 물건으로 수정

--- src/main/java/antigravity/exception/NotFoundProductException.java
+++ src/main/java/antigravity/exception/NotFoundProductException.java
@@ -3,7 +3,7 @@ package antigravity.exception;
 import antigravity.common.BaseException;
 
 public class NotFoundProductException extends BaseException {
-    private final static String MESSAGE = "없는 회원입니다.";
+    private final static String MESSAGE = "없는 물건입니다.";
 
     public NotFoundProductException() {
         super(MESSAGE);
