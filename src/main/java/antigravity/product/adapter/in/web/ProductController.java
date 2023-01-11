a19f75a19ff8be494ef86e377c2ee762a197bab8
Dohun Lee <nestour95@gmail.com>
2023-01-12 00:22:09 +0900

    feat: 상품 조회 웹 어댑터 구현
    기존 어댑터에 통합하여 구현

--- src/main/java/antigravity/product/adapter/in/web/ProductController.java
+++ src/main/java/antigravity/product/adapter/in/web/ProductController.java
@@ -1,16 +1,22 @@
 package antigravity.product.adapter.in.web;
 
 import static org.springframework.http.HttpStatus.CREATED;
+
+import antigravity.product.application.port.in.dto.GetProductsInput;
+import antigravity.product.application.port.in.GetProductsQuery;
 import antigravity.product.application.port.in.dto.LikeProductInput;
 import antigravity.product.application.port.in.LikeProductUseCase;
 import antigravity.product.common.Response;
 import javax.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
 	private final LikeProductUseCase likeProductUseCase;
+	private final GetProductsQuery getProductsQuery;
 
 	// TODO 찜 상품 등록 API
 	@PostMapping("/liked/{productId}")
@@ -31,5 +38,17 @@ public class ProductController {
 	}
 
 	// TODO 찜 상품 조회 API
+	@GetMapping
+	public ResponseEntity getProducts(
+
+		@RequestHeader("X-USER-ID") @NotNull Long userId,
+		@RequestParam(name = "liked", required = false) Boolean liked,
+		Pageable pageable
+	) {
+		return ResponseEntity.ok()
+			.body(Response.success("success",
+				getProductsQuery.getProducts(new GetProductsInput(userId, liked, pageable)))
+			);
+	}
 
 }
