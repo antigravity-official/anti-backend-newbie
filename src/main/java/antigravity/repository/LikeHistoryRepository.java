5ecfbef868ab005695b1a00b7645ad82b6187c00
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-01-11 16:32:43 +0900

    fix(LikeHistoryRepository) : 안되던 메소드 @Params 추가로 문제 해결

--- src/main/java/antigravity/repository/LikeHistoryRepository.java
+++ src/main/java/antigravity/repository/LikeHistoryRepository.java
@@ -3,12 +3,13 @@ package antigravity.repository;
 import antigravity.entity.LikeHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LikeHistoryRepository extends JpaRepository<LikeHistory, Long> {
     boolean existsByMemberIdAndProductId(Long memberId, Long productId);
 
 
-    @Query("select count(h.id) from LikeHistory h where h.product.id = :id")
-    Long getTotalLike(Long productId);
+    @Query("select count(h.id) from LikeHistory h where h.product.id = :productId")
+    Long getTotalLike(@Param("productId") Long productId);
 
 }
