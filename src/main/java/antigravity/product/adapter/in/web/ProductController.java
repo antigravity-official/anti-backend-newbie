853dd28c1b7819db42a64c4e9abca6b42814ea71
Dohun Lee <nestour95@gmail.com>
2023-01-12 00:14:17 +0900

    feat: 찜 등록 웹 어댑터 구현

--- src/main/java/antigravity/product/adapter/in/web/ProductController.java
+++ src/main/java/antigravity/product/adapter/in/web/ProductController.java
@@ -1,9 +1,35 @@
 package antigravity.product.adapter.in.web;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import antigravity.product.application.port.in.dto.LikeProductInput;
+import antigravity.product.application.port.in.LikeProductUseCase;
+import antigravity.product.common.Response;
+import javax.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/products")
+@RestController
 public class ProductController {
 
-    // TODO 찜 상품 등록 API
+	private final LikeProductUseCase likeProductUseCase;
 
-    // TODO 찜 상품 조회 API
+	// TODO 찜 상품 등록 API
+	@PostMapping("/liked/{productId}")
+	public ResponseEntity likeProduct(
+		@RequestHeader("X-USER-ID") @NotNull Long userId,
+		@PathVariable("productId") @NotNull Long productId
+	) {
+		likeProductUseCase.likeProduct(new LikeProductInput(userId, productId));
+		return ResponseEntity.status(CREATED).body(Response.success("상품을 찜했습니다."));
+	}
+
+	// TODO 찜 상품 조회 API
 
 }
