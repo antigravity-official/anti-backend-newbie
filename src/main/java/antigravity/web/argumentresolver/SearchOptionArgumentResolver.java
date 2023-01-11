fb487bc9644e62dbe2eb3e9ea1efba3553d99735
SeokHo-Ham <kalejuice02@gmail.com>
2023-01-11 16:28:42 +0900

    fix: resolveArgument 메서드에서 헤더 대신 파라미터를 조회하도록 수정

--- src/main/java/antigravity/web/argumentresolver/SearchOptionArgumentResolver.java
+++ src/main/java/antigravity/web/argumentresolver/SearchOptionArgumentResolver.java
@@ -2,11 +2,13 @@ package antigravity.web.argumentresolver;
 
 import antigravity.domain.product.LikeStatus;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class SearchOptionArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
@@ -17,7 +19,8 @@ public class SearchOptionArgumentResolver implements HandlerMethodArgumentResolv
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
         NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String likeStatus = webRequest.getHeader("liked");
+        String likeStatus = webRequest.getParameter("liked");
+
         return LikeStatus.findStatus(likeStatus);
     }
 }
