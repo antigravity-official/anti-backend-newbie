2c28ce6bda9d698e335df6edf8b4bc3b72a9440c
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2023-01-11 16:28:05 +0900

    fix(IllegalParameterException) : 메세지 오타 수정

--- src/main/java/antigravity/exception/IllegalParameterException.java
+++ src/main/java/antigravity/exception/IllegalParameterException.java
@@ -3,7 +3,7 @@ package antigravity.exception;
 import antigravity.common.BaseException;
 
 public class IllegalParameterException extends BaseException {
-    private final static String MESSAGE = "잘못된 파라미이터입니다.";
+    private final static String MESSAGE = "잘못된 파라미터입니다.";
 
     public IllegalParameterException() {
         super(MESSAGE);
