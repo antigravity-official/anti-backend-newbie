5a749d437f3ebbaa64e3f26b960a4b7ebe8cc12c
taeyun-work <wmfajr1357@gmail.com>
2023-01-12 00:57:23 +0900

    feat: 구현 완료

---
+++ src/main/java/antigravity/service/ProductService.java
@@ -0,0 +1,48 @@
+package antigravity.service;
+
+
+import antigravity.payload.PostResponse;
+import antigravity.payload.ProductResponse;
+import antigravity.repository.ProductRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    public ResponseEntity<PostResponse> like(long userId, long productId) {
+
+        PostResponse postResponse = new PostResponse();
+        try {
+            productRepository.createLike(userId, productId);
+            productRepository.increaseHits(productId);
+            postResponse.setStatusCode(201);
+            postResponse.setResult(PostResponse.SUCCESS);
+            return new ResponseEntity<PostResponse>(postResponse, HttpStatus.CREATED);
+        } catch (Exception e) {
+            postResponse.setStatusCode(400);
+            postResponse.setResult(PostResponse.FAIL);
+            return new ResponseEntity<PostResponse>(postResponse, HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    public List<ProductResponse> find(Long userId, String liked, String page, String size) {
+        if (liked == null || liked.trim().isEmpty()) {
+            return productRepository.findAll(userId, page, size);
+        }
+
+        if (Boolean.parseBoolean(liked)) {
+            return productRepository.findByLiked(userId);
+        }
+        return productRepository.findByNotLiked(userId);
+    }
+}
+
