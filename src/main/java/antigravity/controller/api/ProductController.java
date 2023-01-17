05969f6bafb45dcf787a5034133464021e5a8c26
sjin <wlstjdeo00@gmail.com>
2023-01-17 12:34:29 +0900

    REFACTOR : ProductController -> controller annotaition 추가

--- src/main/java/antigravity/controller/api/ProductController.java
+++ src/main/java/antigravity/controller/api/ProductController.java
@@ -1,9 +1,17 @@
 package antigravity.controller.api;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/products")
+@Controller
 public class ProductController {
 
-    // TODO 찜 상품 등록 API
+    // TODO: 찜 상품 등록 API
 
-    // TODO 찜 상품 조회 API
+
+
+    // TODO: 찜 상품 조회 API
 
 }
