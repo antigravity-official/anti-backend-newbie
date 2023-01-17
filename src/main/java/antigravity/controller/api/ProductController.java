3afe0153076e6f1714eae6f6ec2d0f59c401bca4
sjin <wlstjdeo00@gmail.com>
2023-01-17 16:56:26 +0900

    FEATURE : 찜 상품 등록 API 템플릿 생성

--- src/main/java/antigravity/controller/api/ProductController.java
+++ src/main/java/antigravity/controller/api/ProductController.java
@@ -1,14 +1,28 @@
 package antigravity.controller.api;
 
 
+import antigravity.payload.APIDataResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RequestMapping("/products")
 @Controller
 public class ProductController {
 
     // TODO: 찜 상품 등록 API
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/liked/{productId}")
+    public APIDataResponse<String> productRegistration(
+            @PathVariable("productId")Long id
+    ) {
+        boolean result = productService.createEvent();
+        return APIDataResponse.of(Boolean.toString(result));
+    }
 
 
 
