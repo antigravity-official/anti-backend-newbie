737640b2f00a3e5e59091b462e6b0db320eca2a3
sjin <wlstjdeo00@gmail.com>
2023-01-17 12:07:30 +0900

    TEST : ProductRepository find 함수 테스트

--- src/test/java/antigravity/repository/ProductRepositoryTests.java
+++ src/test/java/antigravity/repository/ProductRepositoryTests.java
@@ -2,11 +2,13 @@ package antigravity.repository;
 
 import antigravity.entity.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -16,10 +18,18 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository productRepository;
 
+    @DisplayName("[Repository] findById")
     @Test
     public void findByIdTest() {
         Long id = 1L;
         Optional<Product> product = productRepository.findById(id);
+        Assertions.assertNotNull(product);
+    }
+
+    @DisplayName("[Repository] findAll")
+    @Test
+    public void findAll() {
+        List<Product> product = productRepository.findAll();
         Assertions.assertNotNull(product);
     }
 }
